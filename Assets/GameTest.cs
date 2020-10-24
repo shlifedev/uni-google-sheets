e87c7f932b3a760896a70e73212a647a11d921a8
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-10-24 17:56:29 +0900

    .
    .

--- Assets/GameTest.cs
+++ Assets/GameTest.cs
@@ -7,11 +7,7 @@ public class GameTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameTable.Item.Armor.Load();
-        foreach (var v in GameTable.Item.Armor.ArmorList)
-        {
-            Debug.Log(v.index +" , " + v.uiDesc);
-        }
+ 
     }
 
     // Update is called once per frame
