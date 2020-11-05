7469dca55cb40e0e3bf6305bf18d6dc18cd9bbfb
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-11-05 23:50:15 +0900

    Update DataGenerator.cs

--- Assets/ZG/ZG.Core/IO/Generator/DataGenerator.cs
+++ Assets/ZG/ZG.Core/IO/Generator/DataGenerator.cs
@@ -22,47 +22,7 @@ namespace Hamster.ZG.IO.Generator
         /// <returns></returns>
         public string Generate()
         {
-            return Newtonsoft.Json.JsonConvert.SerializeObject(info);
-
-            // verticla data to horizontal data, obsolute//
-
-
-            //StringBuilder sb = new StringBuilder();
-            //StringWriter sw = new StringWriter(sb);
-            //Newtonsoft.Json.JsonWriter writer = new JsonTextWriter(sw);
-            //foreach (var table in info.tableResult)
-            //{
-            //    var className = table.Key; 
-            //    var typeArray = table.Value.Keys.ToList<string>();
-            //    if(typeArray.Count == 0)
-            //        return null;
-            //    var typeCount = table.Value.Keys.Count(); 
-            //    var valueCount = table.Value[typeArray[0]].Count;  
-
-            //    List<List<string>> values = new List<List<string>>();
-            //    for(int typeIdx  = 0; typeIdx < typeCount; typeIdx++)
-            //    {
-            //        var type = typeArray[typeIdx];
-            //        values.Add(table.Value[type]); 
-            //    } 
-            //    writer.WriteStartArray();
-            //    for (int v = 0; v < valueCount; v++)
-            //    {
-            //        writer.WriteStartObject();
-            //        for (int i = 0; i < typeCount; i++)
-            //        {
-            //            var type = typeArray[i];
-            //            var split = type.Replace(" ", null).Split(':'); // split typeName : propertyName
-            //            var value = values[i][v];
-            //            writer.WritePropertyName(split[0]);
-            //            writer.WriteValue(value); 
-            //        }
-            //        writer.WriteEndObject();
-            //    }
-            //    writer.WriteEndArray();
-            //}
-
-            //return sb.ToString();
+            return Newtonsoft.Json.JsonConvert.SerializeObject(info); 
         }
     }
 }
