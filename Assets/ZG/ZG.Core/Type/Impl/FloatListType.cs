07cb8f670651a011aee2c8525631cb1c152fc42a
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-12-08 17:17:12 +0900

    .
    .

---
+++ Assets/ZG/ZG.Core/Type/Impl/FloatListType.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Hamster.ZG.Type
+{
+    [Type(type: typeof(List<float>), speractors: new string[] { "list<float>", "List<Float>" })]
+    public class FloatListType : IType
+    {
+        public object DefaultValue => null;
+        public object Read(string value)
+        { 
+            var list = new System.Collections.Generic.List<float>();
+            if (value == "[]") return list;
+
+            var datas = ReadUtil.GetBracketValueToArray(value);
+            if (datas != null)
+            {
+                foreach (var data in datas)
+                    list.Add(float.Parse(data));
+            }
+            return list;
+        }
+
+        public string Write(object value)
+        {
+            var list = value as List<float>;
+            return WriteUtil.SetValueToBracketArray(list);
+        }
+    }
+}
