011f4f809b3b053da4ea8d0d11b7026f613fc365
Cheese Allergy Hamster <shlifedev@gmail.com>
2021-07-20 06:31:53 +0900

    .
    .

--- App/Program.cs
+++ App/Program.cs
@@ -7,21 +7,19 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void Credential()
         {
             HamsterGoogleSheet.Init(new GSParser(), new FileReader());
             GoogleDriveWebRequester.Instance.password = "dpqlcb123123";
-            GoogleDriveWebRequester.Instance.baseURL = "https://script.google.com/macros/s/AKfycbxEQl5s-Q0qwSRtUEsJF8FcWlrPQVzFlUa4rgFV3XOhVzBCvRA/exec";
-
-            UnitData.Balance.LoadFromGoogle((list, map) => {
-                list.ForEach(x => {
-                    Console.WriteLine(x);
-                }); 
-            }, true);
-
-
+            GoogleDriveWebRequester.Instance.baseURL = "https://script.google.com/macros/s/AKfycbxEQl5s-Q0qwSRtUEsJF8FcWlrPQVzFlUa4rgFV3XOhVzBCvRA/exec"; 
+        }
+        static void Main(string[] args)
+        {
+            Credential(); 
             GoogleDriveWebRequester.Instance.ReadGoogleSpreadSheet("1BXya0YQq980kbNBN_-hQAvmBrNkHFIoqXJkQTXIsXHQ", (err) => { }, (callback, json) => { 
-                HamsterGoogleSheet.DataParser.ParseSheet(json, true, true, new FileWriter());
+                HamsterGoogleSheet.DataParser.ParseSheet(json, true, true, new FileWriter()); 
+                Console.WriteLine(json);
             });
         }
     }
