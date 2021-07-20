14d294efbf14927ceee6fb4c93fd714483bfc9a4
Cheese Allergy Hamster <shlifedev@gmail.com>
2021-07-21 06:20:32 +0900

    Update HamsterGoogleSheet.cs

--- HamsterGoogleSpreadSheet/ZG/ZG.Core/ZG/HamsterGoogleSheet.cs
+++ HamsterGoogleSpreadSheet/ZG/ZG.Core/ZG/HamsterGoogleSheet.cs
@@ -1,40 +1,53 @@
 ﻿ 
 using Hamster.ZG.IO;
+using Hamster.ZG.IO.FileReader;
 using Hamster.ZG.Type;
+using HamsterGoogleSpreadSheet.ZG.ZG.Core.Http.ProtocolV2.Req;
+using HamsterGoogleSpreadSheet.ZG.ZG.Core.Http.ProtocolV2.Res;
 using System; 
 namespace Hamster.ZG
 {
+    public static class GoogleSheet
+    {
+        
+        private static bool _credentialCalled = false;
+        public static void Initialize(string scriptURL, string password)
+        {
+            GoogleDriveWebRequesterV2.Instance.Credential(scriptURL, password);
+            HamsterGoogleSheet.Init(new GSParser(), new FileReader()); 
+        }
+        public static void GetDriveDirectory(string folderID, System.Action<GetDriveFolderResult> callback)
+        {
+            GoogleDriveWebRequesterV2.Instance.GetDriveDirectory(new GetDriveDirectoryReqModel(folderID), OnError, callback);
+        }
+
+        public static void ReadSpreadSheet(string fileID, System.Action<ReadSpreadSheetResult> callback)
+        {
+            GoogleDriveWebRequesterV2.Instance.ReadSpreadSheet(new ReadSpreadSheetReqModel(fileID), OnError, callback);
+        }
+ 
+
+        public static void OnError(System.Exception e)
+        {
+
+        } 
+    }
+
     public static class HamsterGoogleSheet
     {
+    
         private static IParser _dataParser;
         private static IFileReader _dataReader;
         public static IParser DataParser { get => _dataParser; } 
         public static IFileReader DataReader { get => _dataReader;}
-         
+
+
+     
         public static void Init(IParser parser, IFileReader reader)
         { 
             TypeMap.Init();  
             _dataParser = parser;
             _dataReader = reader;
-        }
-         
-        public static object TestRead(string sepractor, string value)
-        {
-            var isExistType = TypeMap.StrMap.ContainsKey(sepractor);
-            if (isExistType)
-            {
-                var refType = TypeMap.Map[TypeMap.StrMap[sepractor]];
-                var data = refType.Read(value);
-                Console.WriteLine("value : " + data);
-                
-                return data;
-            }
-            else
-            {
-                Console.WriteLine("not exist type sepractor => " + sepractor);
-            }
-
-            return null;
-        }
+        } 
     }
 }
